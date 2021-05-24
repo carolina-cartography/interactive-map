140f3dad5591464d0fddf3bc7b8ab74a9bbb3e4e
Tomas Roy <hello@minicreative.net>
2021-05-24 00:48:18 -0400

    mobile navigation support

--- app/components/Header.js
+++ app/components/Header.js
@@ -4,11 +4,17 @@ import { APP_NAME, LANDING, LOGO_LINK } from './../tools/Constants'
 import Authentication from './../tools/Authentication'
 
 export default class Header extends React.Component{
+
+	constructor(props) {
+		super(props)
+		this.toggleMobileNav = this.toggleMobileNav.bind(this)
+	}
 	
 	state = {
 		path: location.pathname,
 		loggedIn: Authentication.isAuthenticated(),
-		isAdmin: Authentication.isAdmin()
+		isAdmin: Authentication.isAdmin(),
+		mobileNavOpen: false,
 	}
 
 	componentDidMount() {
@@ -17,8 +23,13 @@ export default class Header extends React.Component{
 				path: location.pathname,
 				loggedIn: Authentication.isAuthenticated(),
 				isAdmin: Authentication.isAdmin(),
+				mobileNavOpen: false,
 			})
 		})
+	}
+
+	toggleMobileNav() {
+		this.setState({ mobileNavOpen: !this.state.mobileNavOpen })
 	}
 
 	logout() {
@@ -26,22 +37,23 @@ export default class Header extends React.Component{
 	}
 
 	render() {
-		const { path, loggedIn, isAdmin, } = this.state
+		const { path, loggedIn, isAdmin, mobileNavOpen } = this.state
 		return (
 			<header className={loggedIn ? "inside" : null}>
 				<div className={path.includes('/map/') ? "container map-container" : "container"}>
 					<a href={LOGO_LINK}><div className="logo">{APP_NAME}</div></a>
-					<div className="navigation">
-						<div className="menu main_menu">
+					<div className="mobile-nav-toggle" onClick={this.toggleMobileNav}>{"Menu"}</div>
+					<div className={mobileNavOpen ? "navigation expanded" : "navigation"}>
+						<div className="menu main-menu">
 							<NavLink to="/maps">{"Maps"}</NavLink>
 						</div>
 						{loggedIn 
-							? <div className="menu user_menu">
+							? <div className="menu user-menu">
 								{isAdmin && <NavLink to="/new">{"New Map"}</NavLink>}
 								<NavLink to="/settings">{"Settings"}</NavLink>
 								<NavLink className="logout" to={LANDING} onClick={this.logout}>{"Logout"}</NavLink>
 							</div> 
-							: <div className="menu user_menu">
+							: <div className="menu user-menu">
 								<NavLink to="/login">{"Login"}</NavLink>
 								<NavLink to="/signup">{"Sign Up"}</NavLink>
 							</div>}
